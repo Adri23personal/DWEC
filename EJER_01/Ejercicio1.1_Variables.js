e3f5d4eb0046044e2bcd8320e3a3a1ddee65747b
Adri23personal <rwf69373@educastur.es>
2026-09-16 11:09:48 +0200

    Actualizacion nueva

--- EJER_01/Ejercicio1.1_Variables.js
+++ EJER_01/Ejercicio1.1_Variables.js
@@ -1,3 +1,27 @@
-// let nombre
-const nombre = "Adri"
-let edad =23
+// Declaración de variables
+const nombre = "Adriana"
+let edad =22
+
+// Declarar variable con valor booleano
+const tieneMascota = true
+
+// Modificar la edad
+edad = 23
+
+// No permite reasignar un nuevo valor a ‘tieneMascota’ porque es const y no let
+// tieneMascota = false
+
+// edad nombre console.log
+
+// Imprime el valor y el tipo de cada una de las variables, "typeof" te dice el tipo de dato
+console.log(nombre, typeof nombre)
+console.log(edad, typeof edad)
+console.log(tieneMascota, typeof tieneMascota)
+
+// Crear una frase que describa a la persona usando Template Strings
+if (tieneMascota) {
+    console.log(`${nombre} tiene ${edad} años y tiene mascota.`)
+  } else {
+    console.log(`${nombre} tiene ${edad} años y no tiene mascota.`)
+  }
+  
---
+++ EJER_01/Ejercicio1.2_Objetos.js
@@ -0,0 +1,10 @@
+// 1. Crea un objeto ‘coche’ con las propiedades: ‘marca’ (string), ‘modelo’ (string), ‘año’ (number) y ‘estaDisponible’ (boolean).
+let coche = {
+    marca: "Kia",
+    modelo: "Niro",
+    año: 2007,
+    estaDisponible: true
+}
+
+// Muestra el objeto completo en consola utilizando console.table().
+console.table(coche)
