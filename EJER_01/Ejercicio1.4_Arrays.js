f25913f0477b493f0747ccafb87d1d8a1a039f43
Adri23personal <rwf69373@educastur.es>
2026-09-17 12:54:51 +0200

    Comentario

---
+++ EJER_01/Ejercicio1.4_Arrays.js
@@ -0,0 +1,14 @@
+// Crea un array llamado ciudades con los siguientes strings: "Madrid", "Buenos Aires", "Tokio", "Nueva York", "París".
+let ciudades = ["Madrid", "Buenos Aires", "Tokio", "Nueva York", "París"]
+
+// Añade con otra sentencia "Roma" al final del array.
+ciudades.push("Roma")
+
+// Crea un nuevo array ciudadesMayusculas utilizando el método .map() que contenga 
+// todos los elementos del array original pero en mayúsculas.
+let ciudadesMayusculas= ciudades.map(ciudades)
+
+// Crea un nuevo array ciudadesFiltradas utilizando el método .filter() que contenga solo los 
+// elementos que tienen más de 6 caracteres.
+
+// Imprime los tres arrays en consola (ciudades, ciudadesMayusculas, ciudadesFiltradas).
